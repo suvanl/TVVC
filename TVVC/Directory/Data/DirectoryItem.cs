d4d0e1aadbebf98e6242324125f9b8a3681fcc7a
Suvan <mr.slrmx@gmail.com>
2018-04-23 21:12:31 +0100

    Move main helpers to DirectoryStructure class

---
+++ TVVC/Directory/Data/DirectoryItem.cs
@@ -0,0 +1,18 @@
+﻿namespace TVVC.Directory.Data
+{
+    /// <summary>
+    /// Information about directory items, such as drives, files and folders
+    /// </summary>
+    public class DirectoryItem
+    {
+        /// <summary>
+        /// The absolute path to the item
+        /// </summary>
+        public string FullPath { get; set; }
+
+        /// <summary>
+        /// The name of the directory item
+        /// </summary>
+        public string Name { get { return DirectoryStructure.GetFileFolderName(this.FullPath); } }
+    }
+}
---
+++ TVVC/Directory/DirectoryStructure.cs
@@ -0,0 +1,37 @@
+﻿namespace TVVC
+{
+    /// <summary>
+    /// Helper class to query information about directories
+    /// </summary>
+    public static class DirectoryStructure
+    {
+        #region Helpers
+
+        /// <summary>
+        /// Finds the file or folder name from a full path
+        /// </summary>
+        /// <param name="path">The full path</param>
+        /// <returns></returns>
+        public static string GetFileFolderName(string path)
+        {
+            // Returns empty if no path exists
+            if (string.IsNullOrEmpty(path))
+                return string.Empty;
+
+            // Converts all forward slashes to backslashes
+            var normalizedPath = path.Replace('/', '\\');
+
+            // Finds the last backslash in the path
+            var lastIndex = normalizedPath.LastIndexOf('\\');
+
+            // Returns the path itself if no backslash is found
+            if (lastIndex <= 0)
+                return path;
+
+            // Return the file name (after the final backslash)
+            return path.Substring(lastIndex + 1);
+        }
+
+        #endregion
+    }
+}
